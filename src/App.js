2665102210a4f781c1e2a0787b176c962b2cac00
unknown <towfiqu@outlook.com>
2022-07-23 15:04:48 +0600

    Removed some redundant files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Answers from './components/Answers';
 import Signin from './components/Signin';
 import Questions from './components/Questions';
-import AppState from './context/AppState';
 import { isAdmin, isUser, isUserOrAdmin } from './utils/permission';
 import { generateSeedData } from './utils/helper';
 
@@ -11,26 +10,22 @@ function App() {
 
   return (
     <>
-      <AppState>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={getHomePage()} />
-            <Route
-              path='/questions'
-              element={isAdmin ? <Questions /> : <Navigate to='/' replace />}
-            />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={getHomePage()} />
+          <Route
+            path='/questions'
+            element={isAdmin ? <Questions /> : <Navigate to='/' replace />}
+          />
 
-            <Route
-              path='/answers'
-              element={
-                isUserOrAdmin ? <Answers /> : <Navigate to='/' replace />
-              }
-            />
+          <Route
+            path='/answers'
+            element={isUserOrAdmin ? <Answers /> : <Navigate to='/' replace />}
+          />
 
-            <Route path='*' element={<Navigate to='/' replace />} />
-          </Routes>
-        </BrowserRouter>
-      </AppState>
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
